9908cee4ec3be52d4282d23159ce2bcd29b66a2c
cnfairall <101303134+cnfairall@users.noreply.github.com>
2023-10-07 11:53:40 -0500

    jokes work

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,45 @@
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import getJoke from '../api/jokeData';
+import Joke from '../components/Joke';
+
 function Home() {
+  const [joke, setJoke] = useState({});
+  const [btnText, setBtnText] = useState('Get a Joke');
+
+  const setButton = (text) => {
+    setBtnText(text);
+  };
+
+  const getAJoke = () => {
+    getJoke().then((obj) => {
+      setJoke({
+        setup: obj.setup,
+        punchline: obj.delivery,
+      });
+
+      setButton('Get Punchline');
+    });
+  };
+
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Welcome Home!</h1>
-    </div>
+    <>
+      <Joke joke={joke} btnText={btnText} />
+      {btnText === 'Get a Joke' || btnText === 'Get a New Joke' ? (
+        <Button type="button" onClick={getAJoke}>
+          {btnText}
+        </Button>
+      ) : (
+        <Button
+          type="button"
+          onClick={() => {
+            setButton('Get a New Joke');
+          }}
+        >
+          {btnText}
+        </Button>
+      )}
+    </>
   );
 }
 
